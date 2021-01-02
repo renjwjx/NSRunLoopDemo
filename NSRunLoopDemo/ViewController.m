82d01dcf0acc9238a67678145048201817b072dc
Jinwei <renjwjx@gmail.com>
2021-01-02 20:58:13 +0800

    Add Runloop observer

--- NSRunLoopDemo/ViewController.m
+++ NSRunLoopDemo/ViewController.m
@@ -21,12 +21,57 @@
 
 -(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
-//    [self timer1];
+    [self timer1];
     
-    //子线程timter
-    [NSThread detachNewThreadSelector:@selector(timer2) toTarget:self withObject:nil];
+    [self addRunLoopObserver];
+    
+    //create子线程timer
+//    [NSThread detachNewThreadSelector:@selector(timer2) toTarget:self withObject:nil];
 }
 
+/*
+ kCFRunLoopEntry = (1UL << 0),
+ kCFRunLoopBeforeTimers = (1UL << 1),
+ kCFRunLoopBeforeSources = (1UL << 2),
+ kCFRunLoopBeforeWaiting = (1UL << 5),
+ kCFRunLoopAfterWaiting = (1UL << 6),
+ kCFRunLoopExit = (1UL << 7),
+ */
+void observerCallBack(CFRunLoopObserverRef observer, CFRunLoopActivity activity, void *info)
+{
+    switch (activity) {
+        case kCFRunLoopEntry:
+            NSLog(@"即将进入kCFRunLoopEntry");
+            break;
+        case kCFRunLoopBeforeTimers:
+            NSLog(@"即将处理timer kCFRunLoopBeforeTimers");
+            break;
+        case kCFRunLoopBeforeSources:
+            NSLog(@"即将处理source kCFRunLoopBeforeSources");
+            break;
+        case kCFRunLoopBeforeWaiting:
+            NSLog(@"即将进入睡眠 kCFRunLoopBeforeWaiting");
+            break;
+        case kCFRunLoopAfterWaiting:
+            NSLog(@"即将唤醒 kCFRunLoopAfterWaiting");
+            break;
+        case kCFRunLoopExit:
+            NSLog(@"即将退出 kCFRunLoopExit");
+            break;
+            
+        default:
+            break;
+    }
+}
+
+#pragma mark -- 监听状态
+- (void)addRunLoopObserver
+{
+    CFRunLoopObserverRef observer = CFRunLoopObserverCreate(CFAllocatorGetDefault(), kCFRunLoopAllActivities, YES, 0, observerCallBack, nil);
+    CFRunLoopAddObserver(CFRunLoopGetMain(), observer, kCFRunLoopDefaultMode);
+}
+
+#pragma mark -- timer
 //子线程timer
 - (void)timer2
 {
@@ -36,7 +81,7 @@
     }];
     //主动开启runloop
     //不停的run
-//    [[NSRunLoop currentRunLoop] run];
+    [[NSRunLoop currentRunLoop] run];
     // 运行3s， 退出，
 //    [[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:3]];
 }
